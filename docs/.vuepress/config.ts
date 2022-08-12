fc23d7b685720e72dac09f4687fc49f7ae4b00b1
CQBerry <chinq6624@gmail.com>
2022-08-12 11:17:26 +0800

    删除无用包，添加 sitemap 插件，添加 robot.txt，更改 package.json 中 License 为 CC BY-SA 4.0

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,11 +4,21 @@ const { defaultTheme } = require('@vuepress/theme-default');
 
 const { backToTopPlugin } = require('@vuepress/plugin-back-to-top');
 
+// Mathjax3
+
 const { viteBundler } = require('vuepress')
 
 const mathjax = require('markdown-it-mathjax3');
 
 const customElement = ['maction', 'math', 'menclose', 'merror', 'mfenced', 'mfrac', 'mi', 'mmultiscripts', 'mn', 'mo', 'mover', 'mpadded', 'mphantom', 'mroot', 'mrow', 'ms', 'mspace', 'msqrt', 'mstyle', 'msub', 'msubsup', 'msup', 'mtable', 'mtd', 'mtext', 'mtr', 'munder', 'munderover', 'semantics', 'mjx-container', 'g'];
+
+// Algolia Doc Search
+
+// const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
+
+// Sitemap
+
+const { sitemapPlugin } = require('vuepress-plugin-sitemap2');
 
 export default defineUserConfig({
     extendsMarkdown: md => {
@@ -29,6 +39,12 @@ export default defineUserConfig({
     ],
     plugins: [
         backToTopPlugin(),
+        // docsearchPlugin({
+
+        // }),
+        sitemapPlugin({
+            hostname: 'rpe.chinq.xyz'
+        })
     ],
 
     lang: 'zh-CN',
