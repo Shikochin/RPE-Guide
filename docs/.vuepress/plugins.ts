673c645f8b10e1ae099816c9fc3ba829a89cd4ab
CQBerry <chinq6624@gmail.com>
2022-08-19 13:36:49 +0800

    fix: :bug: fix build warning: plugin @vuepress/plugin-back-to-top has been used multiple times

--- docs/.vuepress/plugins.ts
+++ docs/.vuepress/plugins.ts
@@ -1,12 +1,10 @@
 import type { PluginConfig } from 'vuepress';
-import { backToTopPlugin } from '@vuepress/plugin-back-to-top';
 import { sitemapPlugin } from 'vuepress-plugin-sitemap2';
 import { searchPlugin } from '@vuepress/plugin-search'
 import { pwaPlugin } from '@vuepress/plugin-pwa'
 import { pwaPopupPlugin } from '@vuepress/plugin-pwa-popup'
 
 const pluginConfigs: PluginConfig = [
-    backToTopPlugin(),
     sitemapPlugin({
         hostname: 'rpe.chinq.xyz'
     }),
